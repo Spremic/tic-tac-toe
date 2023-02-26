ccf0c74ea9aba0eaf3d928e4b469de4695b02a6d
Spremic <dspremic1@gmail.com>
2023-02-26 16:43:04 +0100

    vercel

--- static/script/user.js
+++ static/script/user.js
@@ -2,7 +2,7 @@ let openSearch = document.querySelector(".divSearch i");
 let searchInput = document.querySelector("#search");
 let writeUsers = document.querySelector(".allUsers");
 let token = localStorage.getItem("token");
-const socket = io();
+// const socket = io();
 // socket.on("connect", () => {
 //   console.log("Radi");
 // });
